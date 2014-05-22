87fa23eceab964cb0c46359406b66c4c027edbc0
Daniel Martins <daniel.tritone@gmail.com>
2014-05-22 08:32:04 -0300

    Fixed package homepage.

--- info.rkt
+++ info.rkt
@@ -3,5 +3,5 @@
 (define name "Transmission RPC Client")
 (define blurb '("Simple Transmission RPC client for Racket."))
 (define version "0.0.1")
-(define homepage "https://github.com/danielfm/racket-transmission-rpc-client")
+(define homepage "https://github.com/danielfm/transmission-rpc-client")
 (define categories '('net))
